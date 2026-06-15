443de0e639490e0f05df867ea271e1127183f4f9
sub1n17 <sub1n17@naver.com>
2026-06-15 12:14:54 +0900

    add ch01

---
+++ ch01/src/main/java/org/example/App.java
@@ -0,0 +1,19 @@
+package org.example;
+
+/**
+ * 260615 1장 스프링 개발환경 구축 실습
+ * 프로젝트 생성
+ * 1) maven project
+ * 2) maven-archetype-quickstart
+ * 3) spring-context 6.2.19 의존성 추가
+ *
+ */
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("hello spring!");
+    }
+}
